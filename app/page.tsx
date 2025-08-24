0ec1d2e5784c0e2e176dc6531d1a2ee045848770
Brody Daniels <cix@Brodys-MacBook-Air.local>
2025-08-24 21:28:17 +0200

    early stage beta.akinn.xyz infra build / Route fix.

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <>
       <PromoBanner 
         message="Introducing Era: your new favorite crypto protocol. Coming soon to Ethereum."
-        href="https://beta.akinn.xyz/" // Point to the beta ERA Protocol
+        href="/beta" // Point to the beta ERA Protocol
       />
       <HalftoneCanvas />
     </>
